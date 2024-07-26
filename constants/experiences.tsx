ed5f143a61ee8409e4ea885e5c72e5f061e503cf
Shahed Ahmed <shahed@shaheds-air.mynetworksettings.com>
2024-07-26 16:38:41 -0400

    Initial Commit

---
+++ constants/experiences.tsx
@@ -0,0 +1,50 @@
+export const EXPERIENCES = [
+    {
+      role: "Software Engineer Fellow",
+      company: "Headstarter",
+      time: "July 2024 - Present",
+      description:
+        "Currently engaged in a 7-week fellowship focused on building AI projects and honing skills in modern web technologies. Participating in weekend hackathons, resume reviews, and interview preparation sessions.",
+      link: "https://headstarter.co/",
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "OpenGym",
+      time: "June 2024 - Present",
+      description:
+        "Developed comprehensive end-to-end tests using Cypress to ensure the quality and stability of the web application. Created a recommendation system that matched users to gyms using advanced algorithms, enhancing user experience and engagement. Utilized a tech stack including Next.js, TypeScript, React, Node.js, and CSS to build and maintain scalable and performant web applications.",
+      link: "https://opengym.club/",
+    },
+    {
+      role: "AI/ML Fellow",
+      company: "Cornell: Break Through Tech",
+      time: "May 2024 - Present",
+      description:
+        "Understanding the ML life cycle and performing exploratory data analysis. Training and optimizing ML models, including hands-on implementation from scratch. Applying ethical and inclusive practices in ML.",
+      link: "https://tech.cornell.edu/impact/break-through-tech/",
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "The City Tutors",
+      time: "Jan 2024 - May 2024",
+      description:
+        "Leveraged Python and Django to diagnose and resolve website issues, enhancing user experience and functionality. Collaborated with cross-functional teams to identify and prioritize software bugs, ensuring efficient resolution within project timelines. Implemented scalable solutions to optimize website performance and streamline processes, contributing to improved overall site functionality and user satisfaction.",
+      link: "https://www.thecitytutors.org/",
+    },
+    {
+      role: "Web Developer",
+      company: "HIPE Network",
+      time: "Feb 2024 - Apr 2024",
+      description:
+        "Developed and launched a fully functional website, incorporating secure account registration and login features. Utilized modern web technologies to create a responsive and user-friendly interface. Deployed the website using Netlify, ensuring seamless deployment and reliable hosting.",
+      link: "https://hipenetwork.net/",
+    },
+    {
+      role: "Research Assistant",
+      company: "CCNY Grove School of Engineering",
+      time: "Sep 2023 - Apr 2024",
+      description:
+        "Enhanced email security by creating and hosting an encrypted email server on a Raspberry Pi using OpenBSD and OpenSMTPD, improving privacy. Optimized client communication by developing custom filters using python, increasing efficiency and client satisfaction. Leveraged Multipurpose Internet Mail Extensions (MIME) and GNU Privacy Guard (GPG) to parse and recognize formats.",
+      link: "https://www.ccny.cuny.edu/engineering",
+    },
+  ];
